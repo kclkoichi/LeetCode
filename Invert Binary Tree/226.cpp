2cb41e79c79df5905da15de21af63b1f64b24c36
Koichi <koichi.ueno@kcl.ac.uk>
2023-02-09 01:42:10 +0800

    Invert Binary Tree

--- Invert Binary Tree/226.cpp
+++ Invert Binary Tree/226.cpp
@@ -10,6 +10,14 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        
+        if(root == nullptr) return nullptr;
+        if(root->left != nullptr || root->right != nullptr) {
+            TreeNode* temp = root->left;
+            root->left = root->right;
+            root->right = temp;
+            invertTree(root->left);
+            invertTree(root->right);
+        }
+        return root;
     }
 };
