a9b33c5d28abc78e48ac51a9720bed6e897a91c1
Koichi <koichi.ueno@kcl.ac.uk>
2023-02-17 03:09:15 +0800

    Group Anagram

--- Group Anagram/49.cpp
+++ Group Anagram/49.cpp
@@ -1,32 +1,18 @@
 #include <map>
 #include <vector>
+#include <algorithm>
 #include <string>
 #include <unordered_map>
 using namespace std;
 
 class Solution {
   public:
-    string countingSort(string s) {
-      string sorted = "";
-      vector<int> counter = vector<int>(26);
-      for(char c : s) counter[c - 'a']++;
-      for(int i = 0; i < counter.size(); i++) {
-        if(counter[i] > 0) {
-          sorted += (char)(i+'a') + to_string(counter[i]);
-        }
-      }
-      return sorted;
-    }
-
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
       unordered_map<string, vector<string> > mapping;
       for(int i = 0; i < strs.size(); i++) {
-        string sorted = countingSort(strs[i]);
-        if(mapping.find(sorted) == mapping.end()) {
-          mapping[sorted] = vector<string> { strs[i] };
-        } else {
-          mapping[sorted].push_back(strs[i]);
-        }
+        string sorted = strs[i];
+        sort(sorted.begin(), sorted.end());
+        mapping[sorted].push_back(strs[i]);
       }
       vector<vector<string>> sol;
       for(auto a : mapping) sol.push_back(a.second);
