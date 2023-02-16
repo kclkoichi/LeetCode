ba4b82703c538653e8722ae45079ec832dc5919b
Koichi <koichi.ueno@kcl.ac.uk>
2023-02-17 03:01:49 +0800

    Counting Sort Group Anagram

--- Group Anagram/49.cpp
+++ Group Anagram/49.cpp
@@ -1,22 +1,33 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
   public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-      map< map<char,int>, vector<string> > mapping;
-      for(int i = 0; i < strs.size(); i++) {
-        map<char,int> counter;
-        for(char c : strs[i]) counter[c]++;
-        if(mapping.find(counter) == mapping.end()) {
-          mapping[counter] = vector<string> { strs[i] };
-        } else {
-          mapping.find(counter)->second.push_back(strs[i]);
+    string countingSort(string s) {
+      string sorted = "";
+      vector<int> counter = vector<int>(26);
+      for(char c : s) counter[c - 'a']++;
+      for(int i = 0; i < counter.size(); i++) {
+        if(counter[i] > 0) {
+          sorted += (char)(i+'a') + to_string(counter[i]);
         }
       }
+      return sorted;
+    }
 
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+      unordered_map<string, vector<string> > mapping;
+      for(int i = 0; i < strs.size(); i++) {
+        string sorted = countingSort(strs[i]);
+        if(mapping.find(sorted) == mapping.end()) {
+          mapping[sorted] = vector<string> { strs[i] };
+        } else {
+          mapping[sorted].push_back(strs[i]);
+        }
+      }
       vector<vector<string>> sol;
       for(auto a : mapping) sol.push_back(a.second);
       return sol;
