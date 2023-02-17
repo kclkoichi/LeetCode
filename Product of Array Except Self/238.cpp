7bcebc5efbacdc805479c8aaa6a55ceb7f4484f3
Koichi <koichi.ueno@kcl.ac.uk>
2023-02-18 02:22:47 +0800

    Product of Array Expect Self

--- Product of Array Except Self/238.cpp
+++ Product of Array Except Self/238.cpp
@@ -4,26 +4,17 @@ using namespace std;
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-      vector<int> fromL = vector<int>(nums.size());
-      int buffer = 1;
+      // multiply everything before with everything after without using more space than the vector to return 
+      vector<int> sol = vector<int>(nums.size(), 1); // every value is initialised to 1
+      int cur = 1;
       for(int i = 0; i < nums.size(); i++) {
-        buffer*=nums[i];
-        fromL[i] = buffer;
+        sol[i] *= cur;
+        cur*=nums[i];
       }
-
-      vector<int> fromR = vector<int>(nums.size());
-      buffer = 1;
+      cur = 1;
       for(int i = nums.size() - 1; i >= 0; i--) {
-        buffer*=nums[i];
-        fromR[i] = buffer;
-      }
-
-      vector<int> sol = vector<int>(nums.size());
-      sol[0] = fromR[0 + 1]; // second
-      sol[nums.size() - 1] = fromL[nums.size()-1 - 1]; // penultimate
-      // Always want multiply everything at its left and everything at its right
-      for(int i = 1; i < nums.size() - 1; i++) {
-        sol[i] = fromL[i - 1] * fromR[i + 1];
+        sol[i] *= cur;
+        cur*=nums[i];
       }
       return sol;
     }
