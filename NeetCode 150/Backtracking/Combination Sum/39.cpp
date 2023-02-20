fb0c659d36b2cc34ed61b0117373a527dff1083c
Koichi <koichi.ueno@kcl.ac.uk>
2023-02-20 22:48:42 +0800

    super slow Combination Sum

--- NeetCode 150/Backtracking/Combination Sum/39.cpp
+++ NeetCode 150/Backtracking/Combination Sum/39.cpp
@@ -17,37 +17,36 @@ public:
       vector<vector<int>> result;
       vector<vector<int>> searching;
       for(int i : candidates) {
-        searching.push_back(vector<int> { i });
+        searching.push_back(vector<int> { i }); // in case a candidate is a solution
         if(i == target) result.push_back(vector<int> { i });
       }
       return bfs(candidates, target, searching, result);
     }
 
     vector<vector<int>> bfs(vector<int>& candidates, int target, vector<vector<int>>&searching, vector<vector<int>>&result) {
-      unordered_set<string> solutions;
+      unordered_set<string> alreadyTried;
       vector<vector<int>> nextSearch;
       while(searching.size() != 0) {
         for(int i = 0; i < searching.size(); i++) {
           for(int j = 0; j < candidates.size(); j++) {
             vector<int> newPotential = searching[i];
             newPotential.push_back(candidates[j]);
-            int sum = evaluateSum(newPotential);
-            if(sum < target) {
-              nextSearch.push_back(newPotential);
-            } else if(sum == target) {
-              // Mapping of counts of numbers
-              map<int, int> mapping;
-              for(int i : newPotential) mapping[i]++;
-              string s = "";
-              for(auto a = mapping.begin(); a != mapping.end(); a++) {
-                s+=to_string(a->first)+to_string(a->second) + ",";
-              }
-              if(solutions.find(s) == solutions.end()) {
-                solutions.insert(s);
+            // Check if we already checked this combination, if yes give up
+            map<int, int> mapping;
+            for(int i : newPotential) mapping[i]++;
+            string s = "";
+            // Map this combination of numbers and their count to a string
+            for(auto a = mapping.begin(); a != mapping.end(); a++) s+=to_string(a->first)+"->"+to_string(a->second) + ",";
+            if(alreadyTried.find(s) == alreadyTried.end()) {
+              alreadyTried.insert(s);
+              int sum = evaluateSum(newPotential);
+              if(sum < target) {
+                nextSearch.push_back(newPotential);
+              } else if(sum == target) {
                 result.push_back(newPotential);
               }
+              // Give up if sum > target
             }
-            // Give up if sum > target
           }
         }
         searching = nextSearch;
