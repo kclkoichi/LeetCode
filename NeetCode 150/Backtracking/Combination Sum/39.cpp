80af30f30e9509c4105141e931b71b2491b6bedd
Koichi <koichi.ueno@kcl.ac.uk>
2023-02-20 23:23:21 +0800

    Combination Sum

--- NeetCode 150/Backtracking/Combination Sum/39.cpp
+++ NeetCode 150/Backtracking/Combination Sum/39.cpp
@@ -1,70 +1,46 @@
 #include <vector>
-#include <iostream>
-#include <unordered_set>
-#include <string>
-#include <map>
 using namespace std;
 
 class Solution {
 public:
-    int evaluateSum(vector<int> v) {
-      int sum = 0;
-      for(int i : v) sum+=i;
-      return sum;
+
+  // We give the index of the candidate to not go back!!
+  // If you notice the state space diagram, for candidate item '2', 
+  // we explore 3 possibilities -> Picking one more '2', Picking. a '3' or picking a '5'. 
+  // But as we move to '3', there are only 2 more possibilities, picking a 3 or a 5,
+  // and finally, for a 5, there is only one possibility -> picking a 5. 
+  // This means that as you go toward the right direction, you don't consider 
+  // the elements towards the left since they already have been taken into account before.
+  void dfs(vector<int>& candidates, int target, vector<vector<int>>& result, vector<int>& currentCombination, int sum, int index) {
+    if(sum > target) return;
+    if(sum == target) {
+      result.push_back(currentCombination);
+      return;
     }
 
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-      vector<vector<int>> result;
-      vector<vector<int>> searching;
-      for(int i : candidates) {
-        searching.push_back(vector<int> { i }); // in case a candidate is a solution
-        if(i == target) result.push_back(vector<int> { i });
-      }
-      return bfs(candidates, target, searching, result);
+    for(int i = index; i < candidates.size(); i++) {
+      currentCombination.push_back(candidates[i]);
+      sum+=candidates[i];
+      dfs(candidates, target, result, currentCombination, sum, i);
+      sum-=candidates[i];
+      currentCombination.pop_back();
     }
 
-    vector<vector<int>> bfs(vector<int>& candidates, int target, vector<vector<int>>&searching, vector<vector<int>>&result) {
-      unordered_set<string> alreadyTried;
-      vector<vector<int>> nextSearch;
-      while(searching.size() != 0) {
-        for(int i = 0; i < searching.size(); i++) {
-          for(int j = 0; j < candidates.size(); j++) {
-            vector<int> newPotential = searching[i];
-            newPotential.push_back(candidates[j]);
-            // Check if we already checked this combination, if yes give up
-            map<int, int> mapping;
-            for(int i : newPotential) mapping[i]++;
-            string s = "";
-            // Map this combination of numbers and their count to a string
-            for(auto a = mapping.begin(); a != mapping.end(); a++) s+=to_string(a->first)+"->"+to_string(a->second) + ",";
-            if(alreadyTried.find(s) == alreadyTried.end()) {
-              alreadyTried.insert(s);
-              int sum = evaluateSum(newPotential);
-              if(sum < target) {
-                nextSearch.push_back(newPotential);
-              } else if(sum == target) {
-                result.push_back(newPotential);
-              }
-              // Give up if sum > target
-            }
-          }
-        }
-        searching = nextSearch;
-        nextSearch.clear();
-      }
-      return result;
-    }
+    return;
+  }
+
+  vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> result;
+    vector<int> currentCombination;
+    dfs(candidates, target, result, currentCombination, 0, 0);
+    return result;
+  }
 };
 
 // LeetCode
 int main() {
   Solution s;
-  vector<int> v { 2,3,5 };
-  for(vector<int> v : s.combinationSum(v, 8)) {
-    for(int i : v) {
-      cout << to_string(i) << " ";
-    }
-    cout << "\n";
-  }
+  vector<int> v {2,3,5};
+  s.combinationSum(v, 8);
   return 0;
 }
