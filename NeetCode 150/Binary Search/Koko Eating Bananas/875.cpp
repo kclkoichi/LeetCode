e7e35dab79501879e40e1d24af7be13dbf6a17cb
Koichi <koichi.ueno@kcl.ac.uk>
2023-02-22 18:18:07 +0800

    Faster Koko Eating Bananas

--- NeetCode 150/Binary Search/Koko Eating Bananas/875.cpp
+++ NeetCode 150/Binary Search/Koko Eating Bananas/875.cpp
@@ -1,33 +1,24 @@
 #include <vector>
-#include <algorithm>
 #include <math.h>
 using namespace std;
 
 class Solution {
 public:
-  bool canEat(vector<int> piles, long long howmuch, int h) {
-    long long count = 0;
-    for(int i = 0; i < piles.size(); i++){
-      count+= ceil(piles[i] / (double)howmuch);
-      if(count > h) return false;
-    }
-    return true;
-  }
-
   int minEatingSpeed(vector<int>& piles, int h) {
-    long long l = 0; long long r = *max_element(piles.begin(), piles.end());
-    while(true) {
-      long long index = (l+r)/2;
-      // We don't use a real array from [1, ...,  n] so we need to index+1 to get the value we want e.g: index 3 is 4
-      if(canEat(piles, index+1, h)) {
-        if(!canEat(piles, index, h)) return index+1;
-        r = index;
+    int l = 1; int r = 1000000000;
+    while(l < r) {
+      int i = (l+r)/2;
+      int count = 0;
+      for(int bananas : piles) count += ceil(bananas / (double) i);
+      if(count > h) {
+        // cannot eat now, need to eat faster
+        l = i+1;
       } else {
-        l = index;
+        r = i;
       }
     }
 
-    return -1;
+    return l;
   }
 };
 
