7f637215668d3cefc500df62fc3ccc54bbaa03d4
Koichi <koichi.ueno@kcl.ac.uk>
2023-02-25 17:44:27 +0800

    Task Scheduler

--- NeetCode 150/Heap (PQ)/Task Scheduler/621.cpp
+++ NeetCode 150/Heap (PQ)/Task Scheduler/621.cpp
@@ -7,44 +7,17 @@ using namespace std;
 class Solution {
 public:
   int leastInterval(vector<char>& tasks, int n) {
-    priority_queue<tuple<int, int, char>> min_pq; // Comparison: 1st int biggest then 2nd int biggest then 3rd char
     unordered_map<char, int> counter;
     for(char c : tasks) counter[c]++;
-    for(auto a : counter) min_pq.push(make_tuple(a.second, 0, a.first)); // how many tasks of this type, time of last execution, task name
-
-    int i = 0;
-    while(!min_pq.empty()) {
-      i++;
-      bool foundExecutable = false;
-      vector<tuple<int, int, char>> temp;
-      tuple<int, int, char> curTask = min_pq.top();
-      int lastCount = INT_MAX;
-      int lastTimeNotExec = INT_MAX;
-      while(!foundExecutable && !min_pq.empty()) {
-        curTask = min_pq.top();
-        // cout << to_string(get<0>(curTask)) << to_string(get<1>(curTask)) << get<2>(curTask) << endl;
-        // 1st execution || time elapsed since last execution > n
-        if(get<1>(curTask) == 0 || i - (get<1>(curTask) * -1) > n) {
-          // can execute task
-          min_pq.pop();
-          counter[get<2>(curTask)]--;
-          if(counter[get<2>(curTask)] > 0) min_pq.push(make_tuple(counter[get<2>(curTask)], i*-1, get<2>(curTask)));
-          foundExecutable = true;
-        } else {
-          // Last time something was not executed, its (not) execution time was smaller AND it had more tasks
-          if(get<1>(curTask)*-1 > lastTimeNotExec && get<0>(curTask) < lastCount) break;
-          // if(get<0>(curTask) < lastCount) break;
-          lastTimeNotExec = min(lastTimeNotExec, get<1>(curTask)*-1);
-          lastCount = min(lastCount, get<0>(curTask));
-          temp.push_back(curTask);
-          min_pq.pop();
-        }
-      }
-      for(auto a : temp) min_pq.push(a);
-      temp.clear();
-    }
-
-    return i;
+    int count = 0; // Max Frequency of any character
+    for(auto a : counter) count = max(count, a.second);
+    // At least: maxfreq char + n breaks + maxfreq char + n breaks + ... 
+    int sol = (count-1)*(1+n);
+    // Add the last occurence of that char (or these, if multiple chars had same frequency)
+    for(auto a : counter) if(count == a.second) sol++;
+    // Maybe the total breaks were not enough to do every tasks (use every chars)...
+    // In that case it's tasks.size()
+    return max(sol, (int)tasks.size());
   }
 };
 
