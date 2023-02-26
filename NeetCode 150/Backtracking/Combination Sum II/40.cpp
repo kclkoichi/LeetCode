5e269414cd6d11c6de98c60368265848ffc45d98
Koichi <koichi.ueno@kcl.ac.uk>
2023-02-26 19:26:07 +0800

    Combination Sum II

--- NeetCode 150/Backtracking/Combination Sum II/40.cpp
+++ NeetCode 150/Backtracking/Combination Sum II/40.cpp
@@ -3,19 +3,21 @@ using namespace std;
 
 class Solution {
 public:
-  void dfs(vector<vector<int>>& sol, int i, vector<int> cur, vector<int>& candidates, int sum, int target, int lastAdded) {
-    if(sum > target) return; // backtrack
-    if(i == candidates.size()) {
-      if(sum == target) sol.push_back(cur);
+  // Copying cur was taking time... use & !!!
+  // Some ways enable to backtrack faster and create less recursion... (this more efficient than previous submission)
+  void dfs(vector<vector<int>>& sol, int start, vector<int>& cur, vector<int>& candidates, int sum, int target) {
+    if(sum == target) {
+      sol.push_back(cur);
       return;
-    }
-    // Create world in which we add the current number
-    cur.push_back(candidates[i]);
-    dfs(sol, i+1, cur, candidates, sum+candidates[i], target, candidates[i]);
-    cur.pop_back();
-    // Create world in which we don't add it IFF the latest added to cur isn't the one we added in the world above
-    if(lastAdded != candidates[i]) {
-      dfs(sol, i+1, cur, candidates, sum, target, lastAdded);
+    } else if(sum > target) {
+      return; // backtrack
+    } else {
+      for(int i = start; i < candidates.size(); i++) {
+        if(i > start && candidates[i] == candidates[i-1]) continue; // Don't work with duplicates more than once
+        cur.push_back(candidates[i]);
+        dfs(sol, i+1, cur, candidates, sum+candidates[i], target);
+        cur.pop_back();
+      }
     }
   }
 
@@ -23,7 +25,7 @@ public:
     sort(candidates.begin(), candidates.end());
     vector<vector<int>> sol;
     vector<int> cur {};
-    dfs(sol, 0, cur, candidates, 0, target, -1);
+    dfs(sol, 0, cur, candidates, 0, target);
     return sol;
   }
 };
