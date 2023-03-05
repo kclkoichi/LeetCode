df37d66363e55bd6d4316e6b486a5b072a84b4cb
Koichi <91293756+kclkoichi@users.noreply.github.com>
2023-03-05 14:14:23 +0800

    Redundant Connection (with UFDS)

--- NeetCode 150/Graphs/Redundant Connection/684.cpp
+++ NeetCode 150/Graphs/Redundant Connection/684.cpp
@@ -1,19 +1,51 @@
 #include <vector>
-#include <unordered_set>
+#include <math.h>
 using namespace std;
+
+typedef vector<int> vi;
+
+class UnionFind {                                // OOP style
+private:
+  vi p, rank, setSize;                           // vi p is the key part
+  int numSets;
+public:
+  UnionFind(int N) {
+    p.assign(N, 0); for (int i = 0; i < N; ++i) p[i] = i;
+    rank.assign(N, 0);                           // optional speedup
+    setSize.assign(N, 1);                        // optional feature
+    numSets = N;                                 // optional feature
+  }
+
+  int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
+  bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
+
+  int numDisjointSets() { return numSets; }      // optional
+  int sizeOfSet(int i) { return setSize[findSet(i)]; } // optional
+
+  void unionSet(int i, int j) {
+    if (isSameSet(i, j)) return;                 // i and j are in same set
+    int x = findSet(i), y = findSet(j);          // find both rep items
+    if (rank[x] > rank[y]) swap(x, y);           // keep x 'shorter' than y
+    p[x] = y;                                    // set x under y
+    if (rank[x] == rank[y]) ++rank[y];           // optional speedup
+    setSize[y] += setSize[x];                    // combine set sizes at y
+    --numSets;                                   // a union reduces numSets
+  }
+};
 
 class Solution {
 public:
   vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+    int n = 0;
+    for(vector<int> edge : edges) n = max(max(edge[0], edge[1]), n);
+
+    UnionFind uf = UnionFind(n);
+    
     vector<int> last = vector<int> { -1, -1 };
-    unordered_set<int> seen;
-    seen.insert(edges[0][0]);
     for(vector<int> edge : edges) {
-      int a = edge[0];
-      int b = edge[1];
-      if(seen.find(a) != seen.end() && seen.find(b) != seen.end()) last = edge; // both already contained
-      seen.insert(a);
-      seen.insert(b);
+      int prev = uf.numDisjointSets();
+      uf.unionSet(edge[0] - 1, edge[1] - 1); // edge are 1-index based, inside UFDS it's 0-index based
+      if(uf.numDisjointSets() == prev) last = edge;
     }
     return last;
   }
