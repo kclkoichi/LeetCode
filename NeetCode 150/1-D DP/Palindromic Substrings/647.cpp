671473a1340066b6cc65136025707eb7acd11a96
Koichi <91293756+kclkoichi@users.noreply.github.com>
2023-03-09 13:57:35 +0800

    Palindromic Substrings

--- NeetCode 150/1-D DP/Palindromic Substrings/647.cpp
+++ NeetCode 150/1-D DP/Palindromic Substrings/647.cpp
@@ -4,21 +4,18 @@ using namespace std;
 
 class Solution {
 public:
-  bool isPalindrome(string s) {
-    int start = 0;
-    int end = s.size()-1;
-    while(start < end) {
-      if(s[start] != s[end]) return false;
-      start++; end--;
-    }
-    return true;
-  }
-
   int countSubstrings(string s) {
     int count = 0;
-    for(int i = s.size(); i >= 0; i--) {
-      for(int j = 0; j+(i-1)<s.size(); j++) {
-        if(isPalindrome(s.substr(j,i))) count++;
+    for(int i = 0; i < s.size(); i++) {
+      // odd
+      int l = i, r = i;
+      while(l >= 0 && r < s.size() && s[r] == s[l]) {
+        count++; l--; r++;
+      }
+      // even
+      l = i, r = i+1;
+      while(l >= 0 && r < s.size() && s[r] == s[l]) {
+        count++; l--; r++;
       }
     }
     return count;
