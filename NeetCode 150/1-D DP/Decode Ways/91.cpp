272ecba5bbd71c16fe10cddf7affb4607f948ece
Koichi <91293756+kclkoichi@users.noreply.github.com>
2023-03-25 17:23:17 +0800

    Decode Ways Top-Bottom (Memorisation)

--- NeetCode 150/1-D DP/Decode Ways/91.cpp
+++ NeetCode 150/1-D DP/Decode Ways/91.cpp
@@ -1,20 +1,25 @@
 #include <string>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
-  int recursion(int i, string& s) {
+  int recursion(int i, string& s, vector<int>& memo) {
     int count = 0;
     // Base Case
     if(i >= s.size()) return 1;
     if(s[i] == '0') return 0;
+    // Use memorisation if possible
+    if(memo[i] != 0) return memo[i];
     // Take one
-    count += recursion(i+1, s);
+    count += recursion(i+1, s, memo);
     // If can add the world where Take two 
-    if((i < s.size() - 1) && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6')) count += recursion(i+2, s);
+    if((i < s.size() - 1) && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6')) count += recursion(i+2, s, memo);
+    memo[i] = count;
     return count;
   }
   int numDecodings(string s) {
-    return recursion(0, s);
+    vector<int> memo = vector<int>(s.size());
+    return recursion(0, s, memo);
   }
 };
