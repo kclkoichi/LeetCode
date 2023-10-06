46d0cd8c7e6272f14567d2980d1f831e643c147a
Koichi <91293756+kclkoichi@users.noreply.github.com>
2023-10-06 17:21:41 +0200

    Step-By-Step Directions From a Binary Tree Node to Another

--- Other Exercises/LeetCode/Step-By-Step Directions From a Binary Tree Node to Another/2096.cpp
+++ Other Exercises/LeetCode/Step-By-Step Directions From a Binary Tree Node to Another/2096.cpp
@@ -1,5 +1,5 @@
 #include <string>
-#include <queue>
+#include <stack>
 using namespace std;
 
 struct TreeNode {
@@ -13,44 +13,52 @@ struct TreeNode {
 
 class Solution {
 public:
-  string getPathFromRootBST(TreeNode* root, int target, string cur) {
-    cur += to_string(root->val);
-    if(root->val == target) return cur;
-    if(target > root->val) 
-      return getPathFromRoot(root->right, target, cur + 'R');
-    else 
-      return getPathFromRoot(root->left, target, cur + 'L');
-  }
-
+  // stack, dfs(preorder), MLE
   string getPathFromRoot(TreeNode* root, int target, string cur) {
-    queue<pair<TreeNode*, string>> q;
-    q.push(make_pair(root, ""));
+    stack<pair<TreeNode*, string>> s;
+    s.push(make_pair(root, ""));
     string path = "";
-    while(!q.empty()) {
-      auto cur = q.front(); q.pop();
-      path = cur.second + to_string(cur.first->val);
+    while(!s.empty()) {
+      auto cur = s.top(); s.pop();
+      path = cur.second;
       if(cur.first->val == target) break;
-      if(cur.first->right != nullptr) q.push(make_pair(cur.first->right, path + 'R'));
-      if(cur.first->left != nullptr) q.push(make_pair(cur.first->left, path + 'L'));
+      if(cur.first->right != nullptr) s.push(make_pair(cur.first->right, path + 'R'));
+      if(cur.first->left != nullptr) s.push(make_pair(cur.first->left, path + 'L'));
     }
     return path;
   }
 
+  // recursion, dfs(preorder), pass
+  bool makePathFromRoot(TreeNode* root, int target, string& cur) {
+    if(root->val == target) return true;
+    if(root->left) {
+      cur += 'L';
+      if(makePathFromRoot(root->left, target, cur)) return true;
+      cur.pop_back();
+    }
+    if(root->right) {
+      cur += 'R';
+      if(makePathFromRoot(root->right, target, cur)) return true;
+      cur.pop_back();
+    }
+    return false;
+  }
+
   string getDirections(TreeNode* root, int startValue, int destValue) {
-    // bfs (because not a BST...)
-    string sPath = getPathFromRoot(root, startValue, "");
-    string dPath =  getPathFromRoot(root, destValue, "");
+    string sPath, dPath;
+    makePathFromRoot(root, startValue, sPath);
+    makePathFromRoot(root, destValue, dPath);
 
     // find right after latest common ancestor
     int i = 0, j = 0;
-    while(sPath[i] == dPath[j]) {
+    while(i < sPath.length() && j < dPath.length() && sPath[i] == dPath[j]) {
       i++;
       j++;
     }
 
     string res = "";
     // Everytime we went R or L, we would go U till Latest Common Ancestor
-    for(char c : sPath.substr(i, sPath.length()-i))
+    for(char c : sPath.substr(i, sPath.length() - i))
       if(c == 'R' || c == 'L') res += 'U';
     // From Latest Common Ancestor we go down till dest
     for(char c : dPath.substr(j, dPath.length() - j))
